5fee678f9317173108b5e9c95854a911c47d7798
Michelle Jdia <mj.sy.ru@gmail.com>
2021-05-31 22:00:53 +0300

    next step change validate func to calss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Card from "../componets/Card.js";
-
+const scrollUpBtn = document.querySelector(".btn-scroll");
 const initialCards = [
   {
     price: "30 000 руб.",
@@ -35,3 +35,55 @@ initialCards.forEach((item)=>{
   const creatCardsElement = elemenCard.generateCard();
   cardsContainer.append(creatCardsElement);
 })
+// function whene the user scrolls down show the button
+window.onscroll = function () {
+  scrollPage();
+};
+
+function scrollPage() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+// function whene the user clicks on the button, page scrolls to the top
+function scrollPageUp() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+scrollUpBtn.addEventListener("click", scrollPageUp);
+
+// validate
+const formInput = document.querySelector(".form");
+const emailInput = formInput.querySelector(".form__input");
+const btnSubmit = formInput.querySelector(".form__btn");
+const error = formInput.querySelector(".footer__input-error");
+
+function checkInputValidity() {
+  if (!emailInput.validity.valid) {
+    error.textContent = emailInput.validationMessage;
+    btnSubmit.disabled = true;
+    btnSubmit.classList.add("footer__btn-submit_disabled");
+  } else {
+    error.textContent = "";
+    btnSubmit.disabled = false;
+    btnSubmit.classList.remove("footer__btn-submit_disabled");
+    disableBtn();
+  }
+}
+
+function disableBtn() {
+  if (emailInput.value.length <= 2) {
+    btnSubmit.disabled = true;
+    btnSubmit.classList.add("footer__btn-submit_disabled");
+  }
+}
+emailInput.addEventListener("input", checkInputValidity);
+formInput.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+disableBtn();
